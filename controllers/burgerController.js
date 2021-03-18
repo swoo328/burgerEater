1ace0030ea10fc947b488c22085d0be4903ad3a4
Steven Woo <swoo9875@gmail.com>
2021-03-18 12:58:46 -0400

    created the routes in burgerController.js

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -14,7 +14,47 @@ router.get("/", function(req, res) {
       res.render("index", hbsObject);
     });
   });
+
 //router.post
+router.post("/api/burgers", function(req, res) {
+    hamburger.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function(result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+//router.put
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    hamburger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 //router.delete
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    hamburger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
