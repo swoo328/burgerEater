32570f6f00f4c13c5f45f8505c60b783e4c2cfd0
Steven Woo <swoo9875@gmail.com>
2021-03-20 15:17:33 -0400

    adding a burger

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,3 +1,61 @@
-//devour button
-//delete button
+$(function () {
+    //devour function 
+    $(".devoured").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+
+        var newDevourState = {
+            devour:newDevour
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function () {
+                console.log("devoured");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+    //adding burgers to the list
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+      var newBurger = {
+          burger_name: $("#ca").val().trim(),
+        //   devoured: $("[name=devoured]:checked").val().trim()
+      };
+
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+      }).then(
+          function() {
+              console.log("created new burger");
+              // Reload the page to get the updated list
+              location.reload();
+          }
+      );
+    });
+//delete function 
+    $(".delete").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
+
 
